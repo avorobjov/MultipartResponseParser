812e835f3505ea523f338ff29c4086d00add996e
Alexander Vorobjov <avorobjov@gmail.com>
2014-07-17 21:48:11 +0300

    Initial Commit

---
+++ MultipartResponseParser/MultipartResponseParser.h
@@ -0,0 +1,13 @@
+//
+//  MultipartResponseParser.h
+//  MultipartResponseParser
+//
+//  Created by Alexander Vorobjov on 17/07/14.
+//  Copyright (c) 2014 Alexander Vorobjov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MultipartResponseParser : NSObject
+
+@end
---
+++ MultipartResponseParser/MultipartResponseParser.m
@@ -0,0 +1,13 @@
+//
+//  MultipartResponseParser.m
+//  MultipartResponseParser
+//
+//  Created by Alexander Vorobjov on 17/07/14.
+//  Copyright (c) 2014 Alexander Vorobjov. All rights reserved.
+//
+
+#import "MultipartResponseParser.h"
+
+@implementation MultipartResponseParser
+
+@end
