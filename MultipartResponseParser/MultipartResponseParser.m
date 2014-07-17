dab6fdfef8710f7eb70ada5071a41a125ebdd194
Alexander Vorobjov <avorobjov@gmail.com>
2014-07-17 22:06:42 +0300

    split parts

--- MultipartResponseParser/MultipartResponseParser.m
+++ MultipartResponseParser/MultipartResponseParser.m
@@ -8,11 +8,47 @@
 
 #import "MultipartResponseParser.h"
 
+@interface MultipartResponseParser ()
+@end
+
 @implementation MultipartResponseParser
+
++ (NSArray *)splitParts:(NSData *)partsData
+{
+    NSUInteger len = partsData.length;
+
+    NSRange boundaryRange = ({
+        NSData *boundaryEnd = [@"\r\n" dataUsingEncoding:NSASCIIStringEncoding];
+        [partsData rangeOfData:boundaryEnd options:0 range:NSMakeRange(0, len)];
+    });
+
+    if (boundaryRange.location == NSNotFound) {
+        return nil; // TODO: no boundary found — wrong separator?
+    }
+
+    NSData *boundary = [partsData subdataWithRange:NSMakeRange(0, boundaryRange.location)];
+
+    NSMutableArray *parts = [[NSMutableArray alloc] init];
+
+    NSUInteger pos = NSMaxRange(boundaryRange);
+    while (pos < len) {
+        NSRange range = [partsData rangeOfData:boundary options:0 range:NSMakeRange(pos, len - pos)];
+        if (range.location == NSNotFound) {
+            break;
+        }
+
+        NSData *part = [partsData subdataWithRange:NSMakeRange(pos, range.location)];
+        [parts addObject:part];
+
+        pos = NSMaxRange(range);
+    }
+
+    return [parts copy];
+}
 
 + (NSArray *)parseData:(NSData *)data
 {
-    return nil;
+    return [self splitParts:data];
 }
 
 @end
